f9ee92054fab98b756e2dc04e86fd33eec02347a
chaitraboggaram <113207424+chaitraboggaram@users.noreply.github.com>
2023-11-18 14:04:22 -0800

    Updating CSS

--- src/modules/booking/payment.jsx
+++ src/modules/booking/payment.jsx
@@ -120,7 +120,7 @@ function Payment() {
   return (
     <div>
       <div className="flight-details-container">
-        <h2>Cost Breakdown</h2>
+        <h2 style={{ color: '#333', textAlign: 'center' }}>Cost Breakdown</h2>
         <table>
           <tbody>
             <tr>
@@ -138,8 +138,7 @@ function Payment() {
               <td>₹{totalAmount}</td>
             </tr>
           </tbody>
-        </table>
-      </div>
+        <br/><br/>
 
       <div className="container">
         <div className="row">
@@ -274,7 +273,8 @@ function Payment() {
           </div>
         </div>
       </div>
-
+      </table>
+      </div>
       <CustomAlert
         title="Payment Successful"
         message="Your payment was successful!"
